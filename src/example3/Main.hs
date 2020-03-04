25d81aae47c6bfe7d76c0982df072d2adfd3ad3f
Steffen Eichenberg <Steffen.Eichenberg@lindenbaum.eu>
2020-03-04 06:25:25 +0100

    clean up

--- src/example3/Main.hs
+++ src/example3/Main.hs
@@ -2,8 +2,6 @@
 {-# LANGUAGE TemplateHaskell  #-}
 module Main where
 
-{- NOTE: The changes in this version aren't my own. Please see https://github.com/KerfuffleV2/haskell-polysemy-test/pull/1 -}
-
 
 import Polysemy
 import Polysemy.Input
@@ -13,9 +11,15 @@ import Polysemy.Trace
 import System.Random (newStdGen, randomRs, randomRIO)
 import Text.Read (readMaybe)
 
+data Config = Config { maxTries :: Int
+                     , lowerBound :: Int
+                     , upperBound :: Int
+                     } deriving Show
 
-newtype GuessConfig = GuessConfig { unGuessConfig :: (Int, (Int, Int)) }
-  deriving Show
+
+--
+-- Interpreters
+--
 
 runListInputForever :: [i] -> Sem (Input i ': r) a -> Sem r a
 runListInputForever is = fmap snd . runState (cycle is) . reinterpret \case
@@ -31,51 +35,63 @@ tracePrefix = intercept \case
   Trace msg -> trace $ "> " ++ msg
 
 
+--
+-- Program
+--
 
-guessProg :: forall r. Members '[Input String, Input Int, Trace] r => GuessConfig -> Sem r [Bool]
+doGuess :: Members '[Input String, Trace] r => Int -> Int -> Int -> Sem r Bool
+doGuess toGuess maxTries remainingTries = do
+  let guessnum = (maxTries - remainingTries) + 1
+  trace $ "Enter guess #" <> show guessnum <> ":"
+  maybeguess <- readMaybe <$> input
+  case maybeguess of
+    Just guess
+      | guess > toGuess -> do
+                             trace (show guess <> " is too high.")
+                             pure False
+      | guess < toGuess -> do
+                             trace (show guess <> " is too low.")
+                             pure False
+      | otherwise -> pure True
+    Nothing -> do
+                 trace "That's not a valid number. You just wasted a guess!"
+                 pure False
+
+
+doGuesses :: Members '[Input String, Trace] r => Int -> Int -> Int -> Sem r Bool
+doGuesses _ _ (-1) = do
+    trace "You win!"
+    pure True
+doGuesses toGuess _ 0 = do
+    trace $ "You ran out of guesses. Game over! The number was " <> show toGuess <> "."
+    pure False
+doGuesses toGuess maxTries remainingTries = do
+    guessed <- doGuess toGuess maxTries remainingTries
+    doGuesses toGuess maxTries (if guessed then (-1) else remainingTries - 1)
+
+
+playAgain :: Members '[Input String, Trace] r => Sem r Bool
+playAgain = do
+    trace ""
+    trace "Play again? (Y/n)"
+    choice <- input
+    pure (elem choice ["y", "yes", "hokay", ""])
+          
+          
+guessProg :: Members '[Input String, Input Int, Trace] r => Config -> Sem r [Bool]
 guessProg config = go True []
   where
-    -- Note: Because of ScopedTypeVariables, r and its constraints are available in the function body.
-    go :: Bool -> [Bool] -> Sem r [Bool]
-    go False record = return $ reverse record
+    go :: Members '[Input String, Input Int, Trace] r => Bool -> [Bool] -> Sem r [Bool]
+    go False record = pure $ reverse record
     go True record = do
-      let GuessConfig (guessesallowed, (minguess, maxguess)) = config
       trace ""
-      trace $ "I'm thinking of a number between " <> show minguess <> " and " <> show maxguess <> ". Guesses allowed: " <> show guessesallowed
-      correct <- input
-      let
-        doGuess :: Int -> Sem r (Maybe Int)
-        doGuess remaining = do
-          let guessnum = (guessesallowed - remaining) + 1
-          trace $ "Enter guess #" <> show guessnum <> ":"
-          maybeguess <- readMaybe <$> input
-          case maybeguess of
-            Just guess
-              | guess > correct -> trace $ show guess <> " is too high."
-              | guess < correct -> trace $ show guess <> " is too low."
-              | otherwise -> return ()
-            Nothing -> trace "That's not a valid number. You just wasted a guess!"
-          return maybeguess
-
-        doGuesses :: Int -> Sem r Bool
-        doGuesses (-1) = trace "You win!" >> return True
-        doGuesses 0 = do
-          trace $ "You ran out of guesses. Game over! The number was " <> show correct <> "."
-          return False
-        doGuesses remaining = do
-          maybeguess <- doGuess remaining
-          doGuesses $ if maybeguess == Just correct then (-1) else remaining - 1
-
-        playAgain :: Sem r Bool
-        playAgain = do
-          trace ""
-          trace "Play again? (Y/n)"
-          (`elem` ["y", "yes", "hokay", ""]) <$> input
-
-      won <- doGuesses guessesallowed
+      trace $ "I'm thinking of a number between " <> show (lowerBound config) <> " and " <> show (upperBound config) <> ". Guesses allowed: " <> show (maxTries config)
+      toGuess <- input
+      won <- doGuesses toGuess (maxTries config) (maxTries config)
       again <- playAgain
       trace $ if again then "Starting a new game!" else "Goodbye!"
       go again (won:record)
+
 
 
 main :: IO ()
@@ -86,28 +102,32 @@ main = do
     guessmin = 1
     guessmax = 100
     bounds = (guessmin, guessmax)
-    conf = GuessConfig (guessesallowed, bounds)
+    conf = Config { maxTries = guessesallowed, lowerBound = guessmin, upperBound = guessmax}
     randlist = randomRs (guessmin, guessmax) rgen
     inputlist = ["50", "75", "", "", "", "y","75"]
+    
     (progoutput :: [String], progreturn)
             = run
-            . runFoldMapOutput (:[])
-            . runTraceAsOutput
+            . runOutputList
+            . traceToOutput
             . tracePrefix
             . runListInputForever randlist
             . runListInputForever inputlist
             $ guessProg conf
 
-  putStrLn "====== Result from pure program"
+  putStrLn "====== Running pure program"
   mapM_ print progoutput
-  putStrLn $ "Successful completion with result: " <> show progreturn
+  putStrLn "====== Result from pure program"
+  putStrLn $ "Completion with result: " <> show progreturn
   putStrLn "\n"
+
+
   putStrLn "====== Running IO program"
   iresult <- runM
-           . runTraceIO
+           . traceToIO
            . tracePrefix
-           . runMonadicInput (sendM $ randomRIO bounds)
-           . runMonadicInput (sendM getLine)
+           . runInputSem (embed $ randomRIO bounds)
+           . runInputSem (embed getLine)
            $ guessProg conf
   putStrLn "====== Result from IO program"
   putStrLn $ "Completion with result: " <> show iresult
