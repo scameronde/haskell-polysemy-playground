35235b31cc9436c7acfb2da90ec523581994d4a2
Steffen Eichenberg <Steffen.Eichenberg@lindenbaum.eu>
2020-03-04 08:32:45 +0100

    Refactor

--- src/example3/Main.hs
+++ src/example3/Main.hs
@@ -21,58 +21,61 @@ data Config = Config { maxTries :: Int
 -- Program
 --
 
-doGuess :: Members '[Input String, Trace] r => Int -> Int -> Int -> Sem r Bool
-doGuess toGuess maxTries remainingTries = do
-  let guessnum = (maxTries - remainingTries) + 1
-  trace $ "Enter guess #" <> show guessnum <> ":"
-  maybeguess <- readMaybe <$> input
-  case maybeguess of
-    Just guess
-      | guess > toGuess -> do
-                             trace (show guess <> " is too high.")
-                             pure False
-      | guess < toGuess -> do
-                             trace (show guess <> " is too low.")
-                             pure False
-      | otherwise -> pure True
-    Nothing -> do
-                 trace "That's not a valid number. You just wasted a guess!"
-                 pure False
 
 
-doGuesses :: Members '[Input String, Trace] r => Int -> Int -> Int -> Sem r Bool
-doGuesses _ _ (-1) = do
-    trace "You win!"
-    pure True
-doGuesses toGuess _ 0 = do
-    trace $ "You ran out of guesses. Game over! The number was " <> show toGuess <> "."
-    pure False
-doGuesses toGuess maxTries remainingTries = do
-    guessed <- doGuess toGuess maxTries remainingTries
-    doGuesses toGuess maxTries (if guessed then (-1) else remainingTries - 1)
 
-
-playAgain :: Members '[Input String, Trace] r => Sem r Bool
-playAgain = do
-    trace ""
-    trace "Play again? (Y/n)"
-    choice <- input
-    pure (elem choice ["y", "yes", "hokay", ""])
-          
           
 guessProg :: Members '[Input String, Input Int, Trace] r => Config -> Sem r [Bool]
 guessProg config = go True []
   where
-    go :: Members '[Input String, Input Int, Trace] r => Bool -> [Bool] -> Sem r [Bool]
-    go False record = pure $ reverse record
+    go :: Members '[Input Int, Input String, Trace] r => Bool -> [Bool] -> Sem r [Bool]
+    go False gameResults = pure (reverse gameResults)
     go True record = do
       trace ""
       trace $ "I'm thinking of a number between " <> show (lowerBound config) <> " and " <> show (upperBound config) <> ". Guesses allowed: " <> show (maxTries config)
-      toGuess <- input
-      won <- doGuesses toGuess (maxTries config) (maxTries config)
-      again <- playAgain
-      trace $ if again then "Starting a new game!" else "Goodbye!"
+      numberToGuess <- input
+      won <- doGuesses numberToGuess (maxTries config) (maxTries config)
+      again <- wantToPlayAgain
+      trace (if again then "Starting a new game!" else "Goodbye!")
       go again (won:record)
+
+
+    doGuesses :: Members '[Input String, Trace] r => Int -> Int -> Int -> Sem r Bool
+    doGuesses _ _ (-1) = do
+        trace "You win!"
+        pure True
+    doGuesses toGuess _ 0 = do
+        trace $ "You ran out of guesses. Game over! The number was " <> show toGuess <> "."
+        pure False
+    doGuesses numberToGuess allowedTries remainingTries = do
+        guessed <- doGuess numberToGuess (allowedTries - remainingTries +1)
+        doGuesses numberToGuess allowedTries (if guessed then (-1) else remainingTries - 1)
+
+
+    doGuess :: Members '[Input String, Trace] r => Int -> Int -> Sem r Bool
+    doGuess numberToGuess guessCount = do
+      trace $ "Enter guess #" <> show guessCount <> ":"
+      maybeguess <- readMaybe <$> input
+      case maybeguess of
+        Just guess
+          | guess > numberToGuess -> do
+                                 trace (show guess <> " is too high.")
+                                 pure False
+          | guess < numberToGuess -> do
+                                 trace (show guess <> " is too low.")
+                                 pure False
+          | otherwise -> pure True
+        Nothing -> do
+                     trace "That's not a valid number. You just wasted a guess!"
+                     pure False
+
+
+    wantToPlayAgain :: Members '[Input String, Trace] r => Sem r Bool
+    wantToPlayAgain = do
+        trace ""
+        trace "Play again? (Y/n)"
+        choice <- input
+        pure (elem choice ["y", "yes", "okay", ""])
 
 
 --
@@ -108,38 +111,39 @@ prefixTrace = intercept \case
 
 main :: IO ()
 main = do
-  rgen <- newStdGen
   let
     guessesallowed = 5
     guessmin = 1
     guessmax = 100
-    bounds = (guessmin, guessmax)
     conf = Config { maxTries = guessesallowed, lowerBound = guessmin, upperBound = guessmax}
-    randlist = randomRs (guessmin, guessmax) rgen
-    inputlist = ["50", "75", "", "", "", "y","75"]
+
+  putStrLn "====== Running pure program"
+  rgen <- newStdGen
+  let
+    listOfNumbersToGuess = randomRs (guessmin, guessmax) rgen
+    listOfCannedAnswers = ["50", "75", "", "", "", "y","75"]
     
-    (progoutput :: [String], progreturn)
+    (progoutput :: [String], pureResult)
             = run
             . runOutputList
             . traceToOutput
             . prefixTrace
-            . runListInputForever randlist
-            . runListInputForever inputlist
+            . runListInputForever listOfNumbersToGuess
+            . runListInputForever listOfCannedAnswers
             $ guessProg conf
-
-  putStrLn "====== Running pure program"
   mapM_ print progoutput
   putStrLn "====== Result from pure program"
-  putStrLn $ "Completion with result: " <> show progreturn
+  putStrLn $ "Completion with result: " <> show pureResult
   putStrLn "\n"
 
 
   putStrLn "====== Running IO program"
-  iresult <- runM
+  ioResult <- runM
            . traceToIO
            . prefixTrace
-           . runInputSem (embed $ randomRIO bounds)
+           . runInputSem (embed $ randomRIO (guessmin, guessmax))
            . runInputSem (embed getLine)
            $ guessProg conf
   putStrLn "====== Result from IO program"
-  putStrLn $ "Completion with result: " <> show iresult
+  putStrLn $ "Completion with result: " <> show ioResult
+
